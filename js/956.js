5780bc4d59549f53017b718d9955b5922bb49555
Serhii Reznichenko <81872621+Sennarion@users.noreply.github.com>
2024-12-15 17:21:32 +0200

    feat: add build

---
+++ js/956.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkglivera_webpack_template=self.webpackChunkglivera_webpack_template||[]).push([[956],{956:(e,a,t)=>{t.r(a),t.d(a,{default:()=>c});const c=function(){}}}]);
--- src/js/utils/constants.js
+++
@@ -1,4 +0,0 @@
-export const BREAKPOINTS = {
-	mediaPoint1: 1024,
-	mediaPoint2: 768,
-};
